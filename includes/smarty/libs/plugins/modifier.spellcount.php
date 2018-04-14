3f81a0d045b488b90a7280344e888872917ffbb2
DS Soft <admin@ds-soft.ru>
2018-04-14 07:07:54 +0300

    [=] Исправлено форматирование;

--- includes/smarty/libs/plugins/modifier.spellcount.php
+++ includes/smarty/libs/plugins/modifier.spellcount.php
@@ -1,5 +1,7 @@
 <?php
-/******************************************************************************/
+
+/* * *************************************************************************** */
+
 //                                                                            //
 //                           InstantCMS v1.10.6                               //
 //                        http://www.instantcms.ru/                           //
@@ -9,15 +11,18 @@
 //                                                                            //
 //                        LICENSED BY GNU/GPL v2                              //
 //                                                                            //
-/******************************************************************************/
-function smarty_modifier_spellcount($num, $one, $two, $many) {
-    if ($num%10==1 && $num%100!=11){
-        echo $num.' '.$one;
+/* * *************************************************************************** */
+function smarty_modifier_spellcount($num, $one, $two, $many, $is_full = 1)
+{
+    if ( $num % 10 == 1 && $num % 100 != 11 ) {
+        $str = $one;
     }
-    elseif($num%10>=2 && $num%10<=4 && ($num%100<10 || $num%100>=20)){
-        echo $num.' '.$two;
+    elseif ( $num % 10 >= 2 && $num % 10 <= 4 && ($num % 100 < 10 || $num % 100 >= 20) ) {
+        $str = $two;
     }
-    else{
-        echo $num.' '.$many;
+    else {
+        $str = $many;
     }
+
+    return ($is_full ? $num . ' ' : '') . $str;
 }
